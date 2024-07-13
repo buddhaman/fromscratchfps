d4c99b82c7877cdbe6b45d7c68c5d202537cc0ae
buddhaman <tim.trussner@gmail.com>
2024-07-13 08:58:31 +0200

    Setup server initialization

--- src/network.h
+++ src/network.h
@@ -8,14 +8,43 @@ typedef struct Network Network;
 struct Network
 {
     WSADATA wsa_data;
+    SOCKET socket;
 };
+
+int 
+CreateSocket(Network* network, I32 port)
+{
+    network->socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(network->socket==INVALID_SOCKET)
+    {
+        fprintf(stderr, "Error creating socket: %ld", WSAGetLastError());
+        WSACleanup();
+        return 0;
+    }
+
+    struct sockaddr_in server_addr;   
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.S_un.S_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
+
+    I32 result = bind(network->socket, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    if(result == SOCKET_ERROR)
+    {
+        fprintf(stderr, "Bind failed: %ld\n", WSAGetLastError());
+        closesocket(network->socket);
+        WSACleanup();
+        return 0;
+    }
+    
+    return 1;
+}
 
 Network* 
 CreateNetwork()
 {
     Network* network;
     network = (Network*)calloc(1, sizeof(*network));
-    int result = WSAStartup(MAKEWORD(2,2), &network->wsa_data);
+    I32 result = WSAStartup(MAKEWORD(2,2), &network->wsa_data);
     if(result != 0)
     {
         fprintf(stderr, "WSAStartup failed: %d", result);
